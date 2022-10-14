edb7d64a6fd4fcdd0f214d6940b40724280ff1e8
zhuzhenquan <2332778528@qq.com>
2022-10-14 21:00:11 +0800

    chore: del @vueuse/nuxt and sass-loader

--- src/views/view1.vue
+++ src/views/view1.vue
@@ -9,8 +9,28 @@ const a = ref(false);
 </script>
 
 <template>
-  <div>view1</div>
+  <div class="div">
+    <div class="div1">div1</div>
+    <div class="div2">div2</div>
+  </div>
+  <el-button-group></el-button-group>
   <el-switch v-model="a" />
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.div {
+  width: 400px;
+  height: 400px;
+  background-color: yellow;
+  .div1 {
+    width: 100px;
+    height: 100px;
+    background-color: blue;
+  }
+  .div2 {
+    width: 100px;
+    height: 100px;
+    background-color: pink;
+  }
+}
+</style>
