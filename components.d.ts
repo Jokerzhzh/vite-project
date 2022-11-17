18b4ca58828929e1a193a9d2864d53072a00b1c6
zhuzhenquan <2332778528@qq.com>
2022-11-17 21:52:44 +0800

    chore: auto import components

--- components.d.ts
+++ components.d.ts
@@ -9,9 +9,15 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElButtonGroup: typeof import('element-plus/es')['ElButtonGroup']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
+    LeftMenu: typeof import('./src/components/LeftMenu/LeftMenu.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    TopBar: typeof import('./src/components/TopBar/TopBar.vue')['default']
   }
 }
